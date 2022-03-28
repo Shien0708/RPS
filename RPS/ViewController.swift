964d2073f8a95c80c7ecc55be8100045997e6342
Shien <f0928652413@gmail.com>
2022-03-28 12:41:49 +0800

    Initial Commit

---
+++ RPS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RPS
+//
+//  Created by 方仕賢 on 2022/3/28.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
